450d22c12f2382c8238b201179170dab06b298a6
lincoln anderson <lincolnanderson@me.com>
2022-04-06 17:37:23 -0500

    better styling

--- ContentView.swift
+++ ContentView.swift
@@ -3,13 +3,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            NBAScoreCardView()
-            
-            NBAScoreCardView()
+            NBAScoreCardView(passedHomeTeam: "MIA", passedAwayTeam: "TOR", passedQuarter: "4TH:", passedHomeTeamScore: 98, passedAwayTeamScore: 102, passedTime: "8:47")
+            NBAScoreCardView(passedHomeTeam: "LAL", passedAwayTeam: "GSW", passedQuarter: "4TH:", passedHomeTeamScore: 98, passedAwayTeamScore: 102, passedTime: "8:47")
         }
     }
 }
--- NBAScoreCardView.swift
+++ NBAScoreCardView.swift
@@ -9,9 +9,47 @@ import Foundation
 import SwiftUI
 
 struct NBAScoreCardView: View {
+    
+    @State var passedHomeTeam: String
+    @State var passedAwayTeam: String
+    @State var passedQuarter: String
+    @State var passedHomeTeamScore: Int
+    @State var passedAwayTeamScore: Int
+    @State var passedTime: String
+    @Environment(\.colorScheme) var colorScheme
     var body: some View {
-        VStack{
-            Text("hello")
+        VStack(spacing: 10){
+            HStack{
+            
+                Text(passedAwayTeam)
+                    .bold()
+                
+                Text("-")
+                    .bold()
+            
+                Text(passedHomeTeam)
+                    .bold()
+                
+            }
+            
+            HStack{
+                Text(passedQuarter)
+                    .bold()
+                Text(passedTime)
+                    .bold()
+            }
+            HStack{
+                Text(String(passedAwayTeamScore))
+                    .bold()
+                Text("-")
+                    .bold()
+                Text(String(passedHomeTeamScore))
+                    .bold()
+            }
         }
+        .padding()
+        .background(Color.gray)
+        .cornerRadius(15)
+        
     }
 }
