f101efb24fd7199ea564f6581d2e881a2dcc57ec
lincoln anderson <lincolnanderson@me.com>
2022-04-06 15:47:07 -0500

    Initial Commit

---
+++ NBAScoreCardView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  NBA Score Viewer
+//
+//  Created by lincoln anderson on 4/6/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct NBAScoreCardView: View {
+    var body: some View {
+        VStack{
+            Text("hello")
+        }
+    }
+}
