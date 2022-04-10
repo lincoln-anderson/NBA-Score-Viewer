e9ef30701fb5c2053b417ed534e69b7cd6af62f8
lincoln anderson <lincolnanderson@me.com>
2022-04-10 16:13:41 -0500

    grid based home screen

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let date: Date
+        
+    init() {
+        date = Date()
+        
+        }
     var body: some View {
-        VStack {
-            NBAScoreCardView(passedHomeTeam: "MIA", passedAwayTeam: "TOR", passedQuarter: "4TH:", passedHomeTeamScore: 98, passedAwayTeamScore: 102, passedTime: "8:47")
-            NBAScoreCardView(passedHomeTeam: "LAL", passedAwayTeam: "GSW", passedQuarter: "4TH:", passedHomeTeamScore: 98, passedAwayTeamScore: 102, passedTime: "8:47")
+        let items = 1...12
+        let columns = [
+            GridItem(.adaptive(minimum: 130, maximum: 130))
+            ]
+        VStack{
+            Text("NBA SCHEDULE FOR \(date, format: .dateTime.day().month())")
+                .bold()
+                .font(.title)
+            ScrollView(.vertical) {
+                LazyVGrid(columns: columns, alignment: .center) {
+                    ForEach(items, id: \.self) { item in
+                        NBAScoreCardView(passedHomeTeam: "MIA", passedAwayTeam: "TOR", passedQuarter: "4TH:", passedHomeTeamScore: 98, passedAwayTeamScore: 102, passedTime: "8:47")
+                        }
+                    }
+            }
         }
     }
 }
